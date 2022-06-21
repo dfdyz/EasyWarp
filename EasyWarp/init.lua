04ae1b59b48693dc0b84b0574dc17aeeb80d847e
dfdyz <72342988+dfdyz@users.noreply.github.com>
2022-06-21 20:25:21 +0800

    Create init.lua

---
+++ EasyWarp/init.lua
@@ -0,0 +1,22 @@
+do
+    local addr, invoke = computer.getBootAddress(), component.invoke
+    local function loadfile(file)
+        local handle = assert(invoke(addr, "open", file))
+        local buffer = ""
+        repeat
+            local data = invoke(addr, "read", handle, math.huge)
+            buffer = buffer .. (data or "")
+        until not data
+        invoke(addr, "close", handle)
+        return load(buffer, "=" .. file, "bt", _G)
+    end
+
+    local function Launch()
+        loadfile("/lib/main.lua")(loadfile)
+    end
+
+    while true do
+        pcall(Launch)
+    end
+
+end
